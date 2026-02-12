4e2b04d2a493251ee1f0fbeabb4828ca51437dac
Mukul bassi <134390152+mukul007d-cole@users.noreply.github.com>
2026-02-12 15:19:37 +0530

    Add server-side error logging hooks

--- src/server.js
+++ src/server.js
@@ -1,4 +1,18 @@
 import app from "./app.js";
 import { PORT } from "./config/constants.js";
 
-app.listen(PORT);
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+server.on("error", (error) => {
+  console.error("Server error:", error);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled promise rejection:", reason);
+});
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught exception:", error);
+});
